7d2a540758ce4d70c704feedc59eecdedc21d6e0
Tpolm <rdt.project@gmail.com>
2020-12-03 21:58:32 +0100

    Updated email message in Jenkins pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -21,7 +21,7 @@ pipeline {
             post {                
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'                    
-                    emailext attachLog: true, body: 'Please go to ${BUILD_URL} and verify the build', compressLog: true, subject: 'Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input', to: 'test@jenkins'
+                    emailext attachLog: true, body: 'Please go to ${BUILD_URL} and verify the build', compressLog: true, subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}", to: 'test@jenkins'
                 }
             }
         }
